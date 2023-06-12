a1d23bf4ddf047b1abd2a92e9763269ee550ccfd
S-15-77 <santhoshdayakar2002@gmail.com>
2023-06-12 11:12:01 +0530

    12/6

---
+++ inp.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+  // Create a string
+  char firstName[30];
+
+  // Ask the user to input some text (name)
+  printf("Enter your first name and press enter: \n");
+
+  // Get and save the text
+  fgets("%s", firstName);
+
+  // Output the text
+  printf("Hello %s", firstName);
+  return 0;
+}
